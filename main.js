138626c2bfd3f8865136fe073b2c3b7bf707be45
DonnyJardine <donnyjardine@gmail.com>
2023-03-16 23:14:29 -0300

    new

---
+++ main.js
@@ -0,0 +1,19 @@
+
+let numero = parseInt(prompt("ingrese numero de integrantes"));
+
+numero = Number(numero);
+
+
+if (numero < 4){
+    alert ("confirmado")
+} else {
+    alert ("lo sentimos... esta cantidad no esta permitida")
+};
+
+
+if (true){
+    alert("bienvenido")
+};
+if (false){
+    alert ("vuelva pronto...")
+};
